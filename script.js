7a3f4945c19aba7e568724ac4d6fa109280bff25
dianesantiago <neidiane.pimenta@gmail.com>
2023-04-22 21:56:34 -0300

    Primeira Versão do meu projeto

---
+++ script.js
@@ -0,0 +1,7 @@
+function Event(){
+if (document.getElementById("nome").value !="" && document.getElementById("email").value !="" && document.getElementById("mensagem").value !="")  {
+alert("prontinho")
+} else {
+  alert("preencha todos os campos")
+}
+}
